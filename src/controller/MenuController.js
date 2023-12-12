71049a6c3c019251b4653cbe109c1d759de6d8ae
yui880 <kimyuna521@gmail.com>
2023-12-12 15:05:32 +0900

    feat(MenuController): 코치의 이름과 못먹는 음식을 가지고 Coach 인스턴스 만드는 기능 구현

--- src/controller/MenuController.js
+++ src/controller/MenuController.js
@@ -1,15 +1,22 @@
 import InputView from '../view/InputView.js';
 import { SEPARATOR } from '../constant/message.js';
+import Coach from '../model/Coach.js';
 
 class MenuController {
   #coachList;
 
+  constructor() {
+    this.#coachList = [];
+  }
+
   async play() {
     const coachNameList = this.#getCoachNames();
+    coachNameList.forEach((coachName) => this.makeCoach(coachName));
   }
 
   async makeCoach(coachName) {
     const inedibleList = this.#getInedibleList(coachName);
+    this.#coachList.push(new Coach(coachName, inedibleList));
   }
 
   async #getCoachNames() {
